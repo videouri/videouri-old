88a87955176baf7252feb42aeb0ae312f96aa7d2
Alexandru Budurovici <w0rldart@w0rldart.com>
2015-02-08 23:12:25 +0000

    temporary fix for GA to load

--- application/views/template.php
+++ application/views/template.php
@@ -44,18 +44,16 @@
 
         <script src="<?= base_url() ?>dist/vendor/modernizr.min.js"></script>
 
-        <?php if (ENVIRONMENT !== 'development'): ?>
-            <script type="text/javascript">
-                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        <script type="text/javascript">
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-                ga('create', 'UA-28752800-1', 'auto');
-                ga('require', 'displayfeatures');
-                ga('send', 'pageview');
-            </script>
-        <?php endif; ?>
+            ga('create', 'UA-28752800-1', 'auto');
+            ga('require', 'displayfeatures');
+            ga('send', 'pageview');
+        </script>
     </head>
 
     <body id="<?= $this->template->bodyId ?>">
